e3d98e149f20e78d29161fc75ac2220a21c75355
lverghese <loganverghese@gmail.com>
2021-09-08 22:41:40 -0400

    foreign keys setup

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,7 @@
 CREATE TABLE department (
     id INTEGER NOT NULL AUTO_INCREMENT,
-    label VARCHAR(30) NOT NULL
+    label VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
 )
 
 
@@ -9,6 +10,7 @@ CREATE TABLE roles (
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     PRIMARY KEY (id),
+    FOREIGN KEY (department_id) INTEGER REFERENCES department(id) ON DELETE CASCADE
 )
 
 
@@ -16,5 +18,9 @@ CREATE TABLE roles (
 CREATE TABLE employee (
      id INTEGER AUTO_INCREMENT PRIMARY KEY,
      first_name VARCHAR(30) NOT NULL,
-     last_name VARCHAR(30) NOT NULL
+     last_name VARCHAR(30) NOT NULL,
+     role_id INT NOT NULL,
+     manager_id INT,
+     FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE,
+     FOREIGN KEY  (manager_id) REFERENCES employee(id) ON DELETE CASCADE
      );
