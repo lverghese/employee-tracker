ce911f37afcfbf8df3e0d45b2e47d1fe4b92dc07
lverghese <loganverghese@gmail.com>
2021-09-08 22:26:14 -0400

    role table and seed

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,15 @@
+
+
+
+CREATE TABLE role (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    PRIMARY KEY (id),
+)
+
+
+
 CREATE TABLE employee (
      id INTEGER AUTO_INCREMENT PRIMARY KEY,
      first_name VARCHAR(30) NOT NULL,
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,16 @@
+INSERT INTO role (title, salary)
+VALUES 
+    ('Lead Engineer', 150000),
+    ('Lawyer', 120000),
+    ('Sales Lead', 75000),
+    ('Sales Manager', 85000),
+    ('Software Engineer', 115000),
+    ('Sales Person', 50000),
+    ('HR Representative', 90000),
+    ('Accountant', 110000)
+  
+
+--Employee Seeds
 INSERT INTO employee (first_name, last_name)
 VALUES
   ('Ronald', 'Firbank'),
