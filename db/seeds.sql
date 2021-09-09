d384699fe80f5ce1b282bf022ed8db0a7c6e1800
lverghese <loganverghese@gmail.com>
2021-09-08 22:00:40 -0400

    init seeds and schema

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO employee (first_name, last_name)
+VALUES
+  ('Ronald', 'Firbank'),
+  ('Virginia', 'Woolf'),
+  ('Piers', 'Gaveston'),
+  ('Charles', 'LeRoi'),
+  ('Katherine', 'Mansfield'),
+  ('Dora', 'Carrington'),
+  ('Edward', 'Bellamy'),
+  ('Montague', 'Summers'),
+  ('Octavia', 'Butler'),
+  ('Unica', 'Zurn');
